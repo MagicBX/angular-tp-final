ace5e9182be35cd99cfd06968f427e0537b06979
Hernan Acosta <soyhernan.acosta@gmail.com>
2023-08-03 23:10:41 -0300

    VFinalAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { PieComponent } from './components/pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PrincipalComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
